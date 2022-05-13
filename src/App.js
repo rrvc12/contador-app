a362a611a272cca2c541fc0f3d6cc4ef180eaba0
rrvc12 <rruilova12@gmail.com>
2022-05-12 21:36:36 -0500

    Se realiza el proyecto en base a los componentes de clase

--- src/App.js
+++ src/App.js
@@ -2,34 +2,73 @@ import './App.css';
 import logo from './images/click.png';
 import Boton from './components/Boton.js';
 import Contador from './components/Contador.js';
-import { useState } from 'react';
+import React from 'react';
 
-function App() {
-  
-  const [numeroClics, setNumeroClics] = useState(0);
 
-  const manejarClic = () => {
-    setNumeroClics(numeroClics+1);
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      numeroClics: 0
+    };
+
+    this.manejarClic = this.manejarClic.bind(this);
+    this.reiniciarContador = this.reiniciarContador.bind(this);
   }
 
-  const reiniciarContador = () => {
-    setNumeroClics(0);
+  manejarClic(){
+    this.setState(({ numeroClics })=>({numeroClics: numeroClics + 1})
+    );
   }
 
-  return (
-    <div className='App'>
-      <div className='logo-contenedor'>
-        <img className='logo-imagen' src={logo} alt='Logo de la Página'/>
-        <h1 className='logo-nombre'>Contador de Clics</h1>
+  reiniciarContador(){
+    this.setState({numeroClics: 0});
+  }
+
+  render(){
+    return (
+      <div className='App'>
+        <div className='logo-contenedor'>
+          <img className='logo-imagen' src={logo} alt='Logo de la Página'/>
+          <h1 className='logo-nombre'>Contador de Clics</h1>
+        </div>
+        <div className='contenedor-principal'>
+        <Contador numeroClics={this.state.numeroClics}/>
+        <Boton texto='Clic' esBotonDeClic={true} manejarClic={this.manejarClic}/>
+        <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={this.reiniciarContador}/>
+        </div>
+        
       </div>
-      <div className='contenedor-principal'>
-      <Contador numeroClics={numeroClics}/>
-      <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic}/>
-      <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={reiniciarContador}/>
-      </div>
-      
-    </div>
-  );
+    );
+  };
 }
+
+// function App() {
+  
+//   const [numeroClics, setNumeroClics] = useState(0);
+
+//   const manejarClic = () => {
+//     setNumeroClics(numeroClics+1);
+//   }
+
+//   const reiniciarContador = () => {
+//     setNumeroClics(0);
+//   }
+
+//   return (
+//     <div className='App'>
+//       <div className='logo-contenedor'>
+//         <img className='logo-imagen' src={logo} alt='Logo de la Página'/>
+//         <h1 className='logo-nombre'>Contador de Clics</h1>
+//       </div>
+//       <div className='contenedor-principal'>
+//       <Contador numeroClics={numeroClics}/>
+//       <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic}/>
+//       <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={reiniciarContador}/>
+//       </div>
+      
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/Contador.css'
 
-function Contador ( {numeroClics} ){
-    return (
-        <div className='contador'>{numeroClics}</div>
-    );
+class Contador extends React.Component {
+    render(){
+        return(
+            <div className='contador'>{this.props.numeroClics}</div>
+        );
+        
+    }
 }
+
 
 export default Contador;
